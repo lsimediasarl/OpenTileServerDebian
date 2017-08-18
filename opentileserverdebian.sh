79cdbd87ed1a5f64bb859381a60823f880878c08
Stephan Bodmer <sbodmer@lsi-media.ch>
2017-08-18 16:32:21 +0200

    Added to style.xml the buffer-size value (to avoid label cuts)

--- opentileserverdebian.sh
+++ opentileserverdebian.sh
@@ -111,7 +111,8 @@ apt clean
 
 # To avoid the label cut between tiles, add the avoid-edges in the default style
 sed -i 's/<Map/<Map\ buffer-size=\"512\"\ /g' /usr/share/openstreetmap-carto/style.xml
-sed -i 's/<ShieldSymbolizer/<ShieldSymbolizer\ avoid-edges=\"true\"\ /g' /usr/share/openstreetmap-carto/style.xml
+# This line is not needed for the moment
+#sed -i 's/<ShieldSymbolizer/<ShieldSymbolizer\ avoid-edges=\"true\"\ /g' /usr/share/openstreetmap-carto/style.xml
 
 #-------------------------------------------------------------------------------
 #--- 2. Create system user
